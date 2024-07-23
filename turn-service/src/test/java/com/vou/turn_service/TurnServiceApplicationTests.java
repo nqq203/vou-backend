e2a2f79af7eede3bdea265f15771b8892d798c52
The Anh <dinhtheanh753@gmail.com>
2024-07-23 15:09:03 +0700

    Add pom and docker-compose

---
+++ turn-service/src/test/java/com/vou/turn_service/TurnServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vou.turn_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
